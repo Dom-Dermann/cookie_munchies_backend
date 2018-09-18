3e5e00942c1f0c55de6cc9a5049b338863936b24
Dom-Dermann <dominikdermann@gmail.com>
2018-09-18 15:47:24 +0200

    added put endpoint

--- routes/items.js
+++ routes/items.js
@@ -7,7 +7,6 @@ const _ = require('lodash');
 // Save new item
 router.post('/', async (req, res) => {
     // validate req body
-
     const result = validate(req.body);
     if (result.error) return res.send(result.error.details[0].message);
 
@@ -23,6 +22,19 @@ router.get('/', async(req, res) => {
     Item.find()
         .then( (i) => res.send(i))
         .catch( (err) => res.send(err));
+});
+
+router.put('/:id', (req, res) => {
+
+    Item.findByIdAndUpdate(req.params.id, {$set: req.body}, (err, i) => {
+        if (err) return res.send(err);
+        res.send(i);
+    })
+})
+
+router.delete('/:id', async(req, res) => {
+    Item.findOneAndRemove({ _id: req.body.id})
+        .then()
 });
 
 
