1c3b3c1b149dc62e8bf268aed237c34d8bbc8de7
Dom-Dermann <dominikdermann@gmail.com>
2018-10-05 12:19:57 +0200

    v1 auto_delete

--- middleware/auto_delete.js
+++ middleware/auto_delete.js
@@ -4,15 +4,12 @@ const auto_delete = async function (req, res, next) {
     // get date from two days ago
     const cutoff = new Date();
     cutoff.setDate(cutoff.getDate()-2);
+    console.log(cutoff);
 
-    // prepare array to hold items
-    let oldItems;
-
-    await Item.find( { dateModified : {$lt: cutoff}})
-        .remove( (err, itms) => {
-            if (err) return res.send(err);
-            console.log(`These items were deleted ${itms} because they exceed the cutoff date of 2 days.`);
-        });
+    await Item.find( {dateStarted: {$gt : cutoff}}, (err, itm) => {
+                if (err) return res.send(err);
+                console.log(itm);
+            });
 
     next();
 }
