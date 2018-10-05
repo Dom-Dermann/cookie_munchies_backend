f88863760a912af86c50e9f1ff046f5ee069a02c
Dom-Dermann <dominikdermann@gmail.com>
2018-10-05 12:28:41 +0200

    auto_delete mw v1.1

--- middleware/auto_delete.js
+++ middleware/auto_delete.js
@@ -4,12 +4,13 @@ const auto_delete = async function (req, res, next) {
     // get date from two days ago
     const cutoff = new Date();
     cutoff.setDate(cutoff.getDate()-2);
-    console.log(cutoff);
+    console.log('Cutoff date', cutoff);
 
-    await Item.find( {dateStarted: {$gt : cutoff}}, (err, itm) => {
+    await Item.find( {dateModified: {$lt : cutoff}, isDone: true}, (err, itm) => {
                 if (err) return res.send(err);
                 console.log(itm);
-            });
+                })
+                .remove();
 
     next();
 }
