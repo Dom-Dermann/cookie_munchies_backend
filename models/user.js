f8ebff79d15ce260f6ab5ce67c39db75f53cd0a6
Dom-Dermann <dominikdermann@gmail.com>
2018-10-19 12:18:17 +0200

    added hashed passwords and password complexity checker

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const Joi = require('joi');
+const PasswordComplexity = require('joi-password-complexity');
 
 const User = mongoose.model('Users', new mongoose.Schema( {
     name: {
@@ -28,7 +30,21 @@ function validateUser(user) {
         email: Joi.string().min(1).max(255).required().email(),
         password: Joi.string().min(1).max(255).required()
     };
+
+    return Joi.validate(user, schema);
 };
+
+function validatePassword(password) {
+    const complexityOptions = {
+        min: 5, 
+        max: 30,
+        lowerCase: 1, 
+        upperCase: 1, 
+        symbol: 1
+    };
+    return Joi.validate(password, new PasswordComplexity(complexityOptions));
+}
 
 module.exports.User = User;
 module.exports.validateUser = validateUser;
+module.exports.validatePassword = validatePassword;
--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,32 @@
-const {User, validateUser}= require('../models/user');
+const {User, validateUser, validatePassword}= require('../models/user');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
 
 router.post('/', async(req, res) => {
     // validate input
     const { error } = validateUser(req.body);
-    if (err) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    // validate password complexity
+    const passwordValidationResult = validatePassword(req.body.password);
+    if (passwordValidationResult.error) return res.status(400).send(passwordValidationResult.error.details[0].message);
 
     // make sure user is not already registered
     let user = await User.findOne( {email: req.body.email });
     if (user) return res.status(400).send('User with this email address already registered.');
 
-    user = new User ({
-        name: req.body.name, 
-        email: req.body.email, 
-        password: req.body.password
-    });
+    user = new User (_.pick(req.body, ['name', 'email', 'password']));
+
+    // hash the password
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
 
     await user.save();
-    res.send(user);
+
+    res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router;
