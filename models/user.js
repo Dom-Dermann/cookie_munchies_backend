5e2e416f549cb6e1a51263333b75bfd859e99f47
Dom-Dermann <dominikdermann@gmail.com>
2018-10-26 10:43:25 +0200

    added user and admin models

--- models/user.js
+++ models/user.js
@@ -23,6 +23,10 @@ const userSchema = new mongoose.Schema( {
         required: true,
         minlength: 1, 
         maxlength: 1024
+    }, 
+    isAdmin: {
+        type: Boolean,
+        default: false
     }
 });
 
@@ -32,7 +36,8 @@ const userSchema = new mongoose.Schema( {
 userSchema.methods.generateAuthToken = function() {
     const token = jwt.sign({
         _id: this._id,
-        name: this.name
+        name: this.name, 
+        isAdmin: this.isAdmin
     }, config.get('jwtPrivateKey'));
     return token;
 }
